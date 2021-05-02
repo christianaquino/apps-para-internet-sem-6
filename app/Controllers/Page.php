d633b058add75b54ee9a0eec92a4674b1a5d2a93
Christian Aquino <christian.a77@gmail.com>
2021-05-01 20:16:37 -0400

    Deleted comment

--- app/Controllers/Page.php
+++ app/Controllers/Page.php
@@ -7,7 +7,6 @@ class Page extends BaseController
 	public function category($page = 'inicio')
 	{
 		if ( ! is_file(APPPATH.'/Views/pages/'.$page.'.php')) {
-        	// hoops, we don't have a page for that!
         	throw new \CodeIgniter\Exceptions\PageNotFoundException($page);
     	}
 
